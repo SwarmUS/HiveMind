f95c4948ee71a9cfa2b0e84ae1d1036560e762a7
xgroleau <xavgroleau@gmail.com>
2020-12-22 20:07:33 -0500

    doc

--- src/bsp/src/stm32/hal/src/uart.c
+++ src/bsp/src/stm32/hal/src/uart.c
@@ -1,6 +1,6 @@
 #include "hal/uart_print.h"
 
-/** @brief Callback on the completion of the transfer */ 
+/** @brief Callback on the completion of the interrupt transfer */ 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef* huart) {
     if (huart == HUART_PRINT) {
         UartPrint_sendTxCallback();
