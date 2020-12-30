a1ce8d7f91142cb82b82ede393241a1efaec1106
xgroleau <xavgroleau@gmail.com>
2020-12-30 13:05:06 -0500

    use logger

--- src/bittybuzz/src/private/BittyBuzzUserFunctions.cpp
+++ src/bittybuzz/src/private/BittyBuzzUserFunctions.cpp
@@ -5,6 +5,6 @@ void bbz_user_functions::logInt() {
     bbzvm_assert_lnum(1); // NOLINT
     bbzobj_t* int_val = bbzheap_obj_at(bbzvm_locals_at(1)); // NOLINT
 
-    printf("Int value: %d \n", int_val->i.value);
+    bbz_system::logger->log(LogLevel::Info, "Int value: %d", int_val->i.value);
     bbzvm_ret0();
 }
