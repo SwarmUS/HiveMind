63715a9b916e143490aae8d2dccc01a0e89ebb3b
xgroleau <xavgroleau@gmail.com>
2021-01-31 23:49:46 -0500

    bump propolis

--- cmake/propolis/common.cmake
+++ cmake/propolis/common.cmake
@@ -13,7 +13,7 @@ function(propolis_fetch_populate)
         ${PROJECT_NAME}_propolis
 
         GIT_REPOSITORY https://github.com/SwarmUS/Propolis
-        GIT_TAG        feat/SWARINFO-154_pheromones-serealization-lib
+        GIT_TAG        21e63e9
         GIT_PROGRESS   TRUE
     )
 
