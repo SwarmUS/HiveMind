821193e9785c3ccdc343a203a4c4912a1d9a47cb
Xavier Groleau <xavgroleau@gmail.com>
2021-02-04 18:01:01 -0500

    fix lwip not releasing memory

--- src/bsp/src/stm32/src/SocketFactory.cpp
+++ src/bsp/src/stm32/src/SocketFactory.cpp
@@ -23,6 +23,7 @@ std::optional<TCPClient> SocketFactory::createTCPClient(const char* address,
     // Connect the socket
     if (lwip_connect(sockfd, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
         logger.log(LogLevel::Info, "Could not connect to server %s : %d", address, port);
+        lwip_close(sockfd);
         return {};
     }
 
