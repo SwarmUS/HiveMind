ce19ce7527677ebe650240015a06814a53133e7f
xgroleau <xavgroleau@gmail.com>
2021-02-06 10:48:00 -0500

    Now using settings container

--- src/logger/src/LoggerContainer.cpp
+++ src/logger/src/LoggerContainer.cpp
@@ -3,7 +3,6 @@
 #include <bsp/SettingsContainer.h>
 
 Logger& LoggerContainer::getLogger() {
-    // TODO: Inject log level, from bsp maybe? TBD
     static Logger s_logger =
         Logger(SettingsContainer::getLogLevel(), BSPContainer::getUserInterface());
     return s_logger;
