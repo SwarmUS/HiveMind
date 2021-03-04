f3915860fb3f7509dd69565c85204d16bfdb781a
Charles Quesnel <charles.quesnel@usherbrooke.ca>
2021-03-04 15:27:48 -0500

    fix compile errors

--- src/bsp/include/bsp/IUSB.h
+++ src/bsp/include/bsp/IUSB.h
@@ -4,10 +4,13 @@
 
 #ifndef HIVE_MIND_IUSB_H
 #define HIVE_MIND_IUSB_H
+
+#include <common/IProtobufStream.h>
+
 class IUSB : public IProtobufStream {
   public:
     virtual ~IUSB() = default;
     virtual bool send(const uint8_t* buffer, uint16_t length) = 0;
     virtual bool receive(uint8_t* buffer, uint16_t length) = 0;
-}
+};
 #endif // HIVE_MIND_IUSB_H
