e5cc27508c789920e789c7848c5d7d95706d91a4
Hubert <dubh3401@usherbrooke.ca>
2021-03-18 11:57:16 -0400

    added forgotten mock file

---
+++ src/bsp/src/posix/include/USBMock.h
@@ -0,0 +1,15 @@
+
+#ifndef HIVE_MIND_USBMOCK_H
+#define HIVE_MIND_USBMOCK_H
+
+class USBMock: public IUSB {
+    public:
+    ~USB() override = default;
+
+    bool send(const uint8_t* buffer, uint16_t length) override {return true;}
+    bool receive(uint8_t* buffer, uint16_t length) override {return true;}
+    bool isConnected() override {return true;}
+
+};
+
+#endif // HIVE_MIND_USBMOCK_H
