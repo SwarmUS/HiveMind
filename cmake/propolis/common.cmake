42b382212b7e92436ac44adee0ed02411531583c
xgroleau <xavgroleau@gmail.com>
2021-03-30 11:48:57 -0400

    bump propolis and rmeoved code associated with swarm api (#61)

--- cmake/propolis/common.cmake
+++ cmake/propolis/common.cmake
@@ -13,7 +13,7 @@ function(propolis_fetch_populate)
         ${PROJECT_NAME}_propolis
 
         GIT_REPOSITORY  https://github.com/SwarmUS/Propolis
-        GIT_TAG         304b74c
+        GIT_TAG         65fad40 
         GIT_PROGRESS    TRUE
     )
 
