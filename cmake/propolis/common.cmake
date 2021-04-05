01c2643c243f08c5e1cda4272510e89c20929aec
xgroleau <xavgroleau@gmail.com>
2021-04-05 10:50:47 -0400

    bump propolis (#69)

--- cmake/propolis/common.cmake
+++ cmake/propolis/common.cmake
@@ -13,7 +13,7 @@ function(propolis_fetch_populate)
         ${PROJECT_NAME}_propolis
 
         GIT_REPOSITORY  https://github.com/SwarmUS/Propolis
-        GIT_TAG         5a92b20
+        GIT_TAG         4eae4a4
         GIT_PROGRESS    TRUE
     )
 
