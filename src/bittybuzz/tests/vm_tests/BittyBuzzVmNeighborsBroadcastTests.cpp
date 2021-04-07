3295ad432839634de24e50161dcb1d1c9d0929c4
xgroleau <xavgroleau@gmail.com>
2021-04-07 15:15:03 -0400

    boardcast tests

--- src/bittybuzz/tests/vm_tests/BittyBuzzVmNeighborsBroadcastTests.cpp
+++ src/bittybuzz/tests/vm_tests/BittyBuzzVmNeighborsBroadcastTests.cpp
@@ -3,13 +3,12 @@
 #include "mocks/BittyBuzzClosureRegisterInterfaceMock.h"
 #include "mocks/BittyBuzzMessageHandlerInterfaceMock.h"
 #include "mocks/BittyBuzzMessageServiceInterfaceMock.h"
+#include "mocks/BittyBuzzNeighborsManagerInterfaceMock.h"
 #include "mocks/BittyBuzzStringResolverInterfaceMock.h"
-#include "mocks/CircularQueueInterfaceMock.h"
-#include "mocks/InterlocInterfaceMock.h"
 #include <bittybuzz/BittyBuzzNeighborsManager.h>
 #include <bittybuzz/BittyBuzzUserFunctions.h>
 #include <gmock/gmock.h>
-#include <neighbors_distance_bytecode.h>
+#include <neighbors_broadcast_bytecode.h>
 
 TEST_F(BittyBuzzVmTestFixture, BittyBuzzVm_neighborsBroadcast) {
     // Given
@@ -18,38 +17,21 @@ TEST_F(BittyBuzzVmTestFixture, BittyBuzzVm_neighborsBroadcast) {
     BittyBuzzStringResolverInterfaceMock stringResolverMock;
     BittyBuzzClosureRegisterInterfaceMock closureRegisterMock;
     BittyBuyzzMessageServiceInterfaceMock messageServiceMock;
-    InterlocInterfaceMock interlocMock;
-    CircularQueueInterfaceMock<uint16_t> queueMock;
-
-    BittyBuzzNeighborsManager neighborsManager(interlocMock, queueMock);
-
-    const uint16_t robotId = 1;
-    RelativePosition pos;
-    pos.m_robotId = robotId;
-    pos.m_distance = 42;
-    pos.m_isInLineOfSight = true;
-    pos.m_relativeOrientation = 24;
+    BittyBuzzNeighborsManagerInterfaceMock neighborsManagerMock;
 
     EXPECT_CALL(messageHandlerMock, messageQueueLength).Times(1).WillOnce(testing::Return(0));
+    EXPECT_CALL(messageServiceMock, sendBuzzMessage).Times(1).WillOnce(testing::Return(0));
 
-    EXPECT_CALL(queueMock, getLength).Times(1).WillOnce(testing::Return(1));
-    EXPECT_CALL(queueMock, peek).Times(1).WillOnce(testing::Return(robotId));
-    EXPECT_CALL(queueMock, pop).Times(1);
-    EXPECT_CALL(interlocMock, getRobotPosition).Times(1).WillOnce(testing::Return(pos));
-
-    std::array<UserFunctionRegister, 1> functionRegister = {
-        {{BBZSTRID_assert_true, buzzAssertTrue}}};
+    std::array<UserFunctionRegister, 0> functionRegister = {};
 
     SetUp(bcode, bcode_size, boardId, &stringResolverMock, &messageHandlerMock,
-          &closureRegisterMock, &messageServiceMock, &neighborsManager, functionRegister);
+          &closureRegisterMock, &messageServiceMock, &neighborsManagerMock, functionRegister);
 
     // Then
 
     m_bittybuzzVm->step();
 
     // Expect
-
-    EXPECT_EQ(g_assertTrueCallCount, 2);
     EXPECT_EQ(vm->state, BBZVM_STATE_READY);
     EXPECT_EQ(vm->error, BBZVM_ERROR_NONE);
 }
