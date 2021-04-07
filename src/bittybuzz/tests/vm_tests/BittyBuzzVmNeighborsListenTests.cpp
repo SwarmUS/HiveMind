3d2e279ddc3e7c257667d9007245080667306b0a
xgroleau <xavgroleau@gmail.com>
2021-04-07 15:31:54 -0400

    wip

---
+++ src/bittybuzz/tests/vm_tests/BittyBuzzVmNeighborsListenTests.cpp
@@ -0,0 +1,46 @@
+#include "BittyBuzzVmFixture.h"
+#include "BittyBuzzVmTestsUtils.h"
+#include "mocks/BittyBuzzClosureRegisterInterfaceMock.h"
+#include "mocks/BittyBuzzMessageHandlerInterfaceMock.h"
+#include "mocks/BittyBuzzMessageServiceInterfaceMock.h"
+#include "mocks/BittyBuzzNeighborsManagerInterfaceMock.h"
+#include "mocks/BittyBuzzStringResolverInterfaceMock.h"
+#include <bittybuzz/BittyBuzzNeighborsManager.h>
+#include <bittybuzz/BittyBuzzUserFunctions.h>
+#include <gmock/gmock.h>
+#include <neighbors_listen_bytecode.h>
+
+TEST_F(BittyBuzzVmTestFixture, BittyBuzzVm_neighborsListen) {
+    // Given
+    uint16_t boardId = 42;
+    BittyBuzzMessageHandlerInterfaceMock messageHandlerMock;
+    BittyBuzzStringResolverInterfaceMock stringResolverMock;
+    BittyBuzzClosureRegisterInterfaceMock closureRegisterMock;
+    BittyBuyzzMessageServiceInterfaceMock messageServiceMock;
+    BittyBuzzNeighborsManagerInterfaceMock neighborsManagerMock;
+
+    EXPECT_CALL(messageHandlerMock, messageQueueLength).Times(1).WillOnce(testing::Return(0));
+    EXPECT_CALL(messageServiceMock, sendBuzzMessage).Times(1).WillOnce(testing::Return(0));
+
+    std::array<uint8_t, 8> payload = {0, 1, 0, 8, 0, 41, 42, 0};
+    bbzmsg_payload_t broadcastMessage;
+    broadcastMessage.buffer = payload.data();
+    broadcastMessage.capacity = 8;
+    broadcastMessage.elsize = 1;
+    broadcastMessage.datastart = 0;
+    broadcastMessage.dataend = 8;
+
+    std::array<UserFunctionRegister, 1> functionRegister = {
+        {{BBZSTRID_assert_true, buzzAssertTrue}}};
+
+    SetUp(bcode, bcode_size, boardId, &stringResolverMock, &messageHandlerMock,
+          &closureRegisterMock, &messageServiceMock, &neighborsManagerMock, functionRegister);
+
+    bbzinmsg_queue_append(&payload);
+    // Then
+    m_bittybuzzVm->step();
+
+    // Expect
+    EXPECT_EQ(vm->state, BBZVM_STATE_READY);
+    EXPECT_EQ(vm->error, BBZVM_ERROR_NONE);
+}
