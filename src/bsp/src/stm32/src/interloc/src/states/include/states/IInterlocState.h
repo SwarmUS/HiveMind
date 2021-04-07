14baa4324e44dca22dbc122933bafb7a1a828497
Charles Quesnel <charles.quesnel@usherbrooke.ca>
2021-04-07 07:18:11 -0400

    state interface

--- src/bsp/src/stm32/src/interloc/src/states/include/states/IInterlocState.h
+++ src/bsp/src/stm32/src/interloc/src/states/include/states/IInterlocState.h
@@ -1,9 +1,13 @@
 #ifndef __IINTERLOCSTATE_H__
 #define __IINTERLOCSTATE_H__
 
+#include <interloc/InterlocManager.h>
+
 class IInterlocState {
   public:
-    virtual void process() = 0;
+    virtual void enter(InterlocManager& context) = 0;
+    virtual void process(InterlocManager& context) = 0;
+    virtual void exit(InterlocManager& context) = 0;
 };
 
 #endif //__IINTERLOCSTATE_H__
