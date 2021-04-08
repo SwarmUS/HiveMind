3efcff26dabba101c6d46e103c9c6b39fabfbb05
Xavier Groleau <xavgroleau@gmail.com>
2021-04-08 16:54:19 -0400

    added interface mock

---
+++ src/message-handler/tests/mocks/InterlocInterfaceMock.h
@@ -0,0 +1,20 @@
+#ifndef __INTERLOCINTERFACEMOCK_H_
+#define __INTERLOCINTERFACEMOCK_H_
+
+#include <gmock/gmock.h>
+#include <interloc/IInterloc.h>
+
+class InterlocInterfaceMock : public IInterloc {
+  public:
+    virtual ~InterlocInterfaceMock() = default;
+
+    MOCK_METHOD(std::optional<RelativePosition>,
+                getRobotPosition,
+                (uint16_t robotId),
+                (const override));
+    MOCK_METHOD(bool, isLineOfSight, (uint16_t robotId), (const override));
+
+    MOCK_METHOD(const PositionsTable&, getPositionsTable, (), (const override));
+};
+
+#endif // __INTERLOCINTERFACEMOCK_H_
