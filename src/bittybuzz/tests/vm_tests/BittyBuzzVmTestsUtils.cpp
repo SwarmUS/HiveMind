8ae9fa5632467c9612d35aa519e50f5297c0ac46
xgroleau <xavgroleau@gmail.com>
2021-04-12 16:07:04 -0400

    Remove unsued test var

--- src/bittybuzz/tests/vm_tests/BittyBuzzVmTestsUtils.cpp
+++ src/bittybuzz/tests/vm_tests/BittyBuzzVmTestsUtils.cpp
@@ -10,8 +10,6 @@ void buzzAssertTrue() {
 
     bbzvm_assert_lnum(1); // NOLINT
     bbzobj_t* bbzObj = bbzheap_obj_at(bbzvm_locals_at(1)); // NOLINT
-    auto test = bbzfloat_tofloat(bbzObj->f.value);
-    (void) test;
 
     EXPECT_TRUE(bbzObj->i.value);
 
