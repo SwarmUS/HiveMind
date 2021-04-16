f33781167fb1e8aae457e3ac67fd0cb2ea4e6aaa
Hubert <dubh3401@usherbrooke.ca>
2021-04-16 15:08:45 -0400

    wip

--- src/bsp/src/stm32/src/interloc/src/states/src/WaitResponseState.cpp
+++ src/bsp/src/stm32/src/interloc/src/states/src/WaitResponseState.cpp
@@ -16,24 +16,41 @@ void WaitResponseState::process(InterlocStateHandler& context) {
 
 
 m_decawaves[DecawavePort::A].receive(m_rxFrame, 2000);
-
+    //getRxStartTime()
     // TODO add multiple receive logic
     if (m_rxFrame.m_status == UWBRxStatus::FINISHED &&
         reinterpret_cast<UWBMessages::DWFrame*>(m_rxFrame.m_rxBuffer.data())->m_functionCode ==
             UWBMessages::FunctionCode::TWR_RESPONSE) {
 
         m_decawaves[DecawavePort::A].getTxTimestamp(&context.getTWR().m_pollTxTs);
+        uint8_t index =  reinterpret_cast<UWBMessages::TWRResponse*>(m_rxFrame.m_rxBuffer.data())
+                             ->m_subFrameId -
+                         1;
         context.getTWR()
-            .m_responseRxTs[reinterpret_cast<UWBMessages::TWRResponse*>(m_rxFrame.m_rxBuffer.data())
-                                ->m_subFrameId -
-                            1] = m_rxFrame.m_rxTimestamp;
+            .m_responseRxTs[index] = m_rxFrame.m_rxTimestamp;
 
-        context.setState(InterlocStates::SEND_FINAL, InterlocEvent::RX_LAST_RESP);
+        nbTries ++;
+
+        if (index == MAX_INTERLOC_SUBFRAMES || nbTries >= MAX_INTERLOC_SUBFRAMES) {
+            nbTries ++;
+            context.setState(InterlocStates::SEND_FINAL, InterlocEvent::RX_LAST_RESP);
+        }
+        // this was not the last expected Rx, continue to receive
+        context.setState(InterlocStates::WAIT_RESPONSE, InterlocEvent::RX_RESP_GOOD);
     } else if (m_rxFrame.m_status == UWBRxStatus::TIMEOUT) {
-        context.setState(InterlocStates::SEND_POLL, InterlocEvent::TIMEOUT);
+        context.getTWR().m_responseRxTs[nbTries ++] = m_rxFrame.m_rxTimestamp;
+
+        if(nbTries >= MAX_INTERLOC_SUBFRAMES){
+            nbTries = 0;
+            context.setState(InterlocStates::SEND_FINAL, InterlocEvent::RX_LAST_TIMEOUT);
+        }
+        context.setState(InterlocStates::WAIT_RESPONSE, InterlocEvent::TIMEOUT);
     } else {
         // Wait a little and send next poll
         Task::delay(100);
         context.setState(InterlocStates::SEND_POLL, InterlocEvent::RX_ERROR);
+        while(1){
+
+        }
     }
 }
