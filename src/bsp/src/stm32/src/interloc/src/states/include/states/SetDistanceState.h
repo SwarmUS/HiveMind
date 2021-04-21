499b32eb0bcad44c018d5aa5ce0a70467a9fb12d
Hubert <dubh3401@usherbrooke.ca>
2021-04-21 11:27:12 -0400

    PR Ready

--- src/bsp/src/stm32/src/interloc/src/states/include/states/SetDistanceState.h
+++ src/bsp/src/stm32/src/interloc/src/states/include/states/SetDistanceState.h
@@ -2,17 +2,12 @@
 #define HIVE_MIND_SETDISTANCESTATE_H
 
 #include "AbstractInterlocState.h"
-#include <interloc/UWBMessages.h>
-#include <interloc/UWBRxFrame.h>
 
 class SetDistanceState : public AbstractInterlocState {
   public:
     SetDistanceState(ILogger& logger, DecawaveArray& decawaves);
 
     void process(InterlocStateHandler& context) override;
-
-  private:
-    //    UWBMessages::TWRResponse m_respMsg{};
 };
 
 #endif // HIVE_MIND_SETDISTANCESTATE_H
