6c7604e0f8e58a30bd7a51c8bd71b3a03ddd2b1e
Charles Quesnel <31734172+cquesnel@users.noreply.github.com>
2021-04-25 14:49:24 -0400

    Swarinfo 366  UUID in flash (#99)
    * Make flash size configurable in CMake
    
    * Save / load UUID from flash
    
    * Update readme
    
    * PR

---
+++ src/bsp/src/stm32/hal/src/hal_flash.c
@@ -0,0 +1,37 @@
+#include "hal/hal_flash.h"
+
+bool Flash_eraseSector(uint8_t sector) {
+    FLASH_EraseInitTypeDef eraseConfig = {.TypeErase = FLASH_TYPEERASE_SECTORS,
+                                          .Sector = sector,
+                                          .NbSectors = 1,
+                                          .VoltageRange = FLASH_VOLTAGE_RANGE_3};
+    uint32_t badSector = 0;
+
+    HAL_FLASH_Unlock();
+    bool ret = HAL_FLASHEx_Erase(&eraseConfig, &badSector) == HAL_OK;
+    HAL_FLASH_Lock();
+
+    return ret;
+}
+
+bool Flash_program(uint32_t address, uint8_t* data, uint32_t size) {
+    if (data == NULL) {
+        return false;
+    }
+
+    HAL_FLASH_Unlock();
+
+    for (unsigned int i = 0; i < size; i++) {
+        if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE, address, (uint64_t)(*data)) != HAL_OK) {
+            HAL_FLASH_Lock();
+            return false;
+        }
+
+        address++;
+        data++;
+    }
+
+    HAL_FLASH_Lock();
+
+    return true;
+}
--- src/bsp/src/stm32/src/BSP.cpp
+++ src/bsp/src/stm32/src/BSP.cpp
@@ -1,15 +1,18 @@
 #include "BSP.h"
 #include "bsp/SettingsContainer.h"
 #include <hal/hal.h>
+#include <logger/LoggerContainer.h>
 
+BSP::BSP() : m_storage(LoggerContainer::getLogger()) {}
 BSP::~BSP() = default;
 
 void BSP::initChip(void* args) {
     (void)args;
 
     Hal_init();
+    m_storage.loadFromFlash();
 }
 
-uint16_t BSP::getUUId() const { return SettingsContainer::getUUID(); }
+uint16_t BSP::getUUId() const { return m_storage.getUUID(); }
 
 uint32_t BSP::generateRandomNumber() { return Hal_generateRandomNumber(); }
