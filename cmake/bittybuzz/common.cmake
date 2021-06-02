00a32d0a28a00731239ea6206394e1503c915cda
xgroleau <xavgroleau@gmail.com>
2021-06-02 19:27:58 -0400

    bump bittybuzz

--- cmake/bittybuzz/common.cmake
+++ cmake/bittybuzz/common.cmake
@@ -6,8 +6,8 @@ set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} PARENT_SCOPE)
 function(bittybuzz_fetch)
     FetchContent_Declare(
         bittybuzz
-        GIT_REPOSITORY https://github.com/xgroleau/BittyBuzz
-        GIT_TAG        42cd16de3018639b8eb11179c318be8c5938864e
+        GIT_REPOSITORY https://github.com/MISTLab/BittyBuzz
+        GIT_TAG        0015fa92468533dddb178e6ba14c7b850ad0cc0e
         GIT_PROGRESS   TRUE
     ) 
 
