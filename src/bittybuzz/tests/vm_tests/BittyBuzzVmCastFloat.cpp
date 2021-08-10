b1a861b8342df513e3ca7dbc1be8827a48aa3996
xgroleau <xavgroleau@gmail.com>
2021-08-10 18:37:25 -0400

    Added tests

---
+++ src/bittybuzz/tests/vm_tests/BittyBuzzVmCastFloat.cpp
@@ -0,0 +1,45 @@
+#include "BittyBuzzVmFixture.h"
+#include "BittyBuzzVmTestsUtils.h"
+#include "mocks/BittyBuzzClosureRegisterInterfaceMock.h"
+#include "mocks/BittyBuzzMessageHandlerInterfaceMock.h"
+#include "mocks/BittyBuzzMessageServiceInterfaceMock.h"
+#include "mocks/BittyBuzzNeighborsManagerInterfaceMock.h"
+#include "mocks/BittyBuzzStringResolverInterfaceMock.h"
+#include <bittybuzz/BittyBuzzLib.h>
+#include <bittybuzz/BittyBuzzUserFunctions.h>
+#include <cast_float_bytecode.h>
+#include <gmock/gmock.h>
+
+TEST_F(BittyBuzzVmTestFixture, BittyBuzzVm_cast_float) {
+    // Given
+    uint16_t boardId = 42;
+    BittyBuzzMessageHandlerInterfaceMock messageHandlerMock;
+    BittyBuzzStringResolverInterfaceMock stringResolverMock;
+    BittyBuzzClosureRegisterInterfaceMock closureRegisterMock;
+    BittyBuyzzMessageServiceInterfaceMock messageServiceMock;
+    BittyBuzzNeighborsManagerInterfaceMock neighborsManagerMock;
+
+    EXPECT_CALL(neighborsManagerMock, updateNeighbors).Times(1);
+    EXPECT_CALL(messageHandlerMock, messageQueueLength).Times(1).WillOnce(testing::Return(0));
+
+    std::array<BittyBuzzLibMemberRegister, 2> functionRegisters = {
+        {{BBZSTRID_float, BittyBuzzUserFunctions::isFloat},
+         {BBZSTRID_assert_true, buzzAssertTrue}}};
+    BittyBuzzLib globalLib(functionRegisters);
+
+    std::vector<std::reference_wrapper<IBittyBuzzLib>> libraries;
+    libraries.emplace_back(globalLib);
+
+    SetUp(bcode, bcode_size, boardId, &stringResolverMock, &messageHandlerMock,
+          &closureRegisterMock, &messageServiceMock, &neighborsManagerMock, libraries);
+
+    // Then
+    m_bittybuzzVm->step();
+
+    // Expect
+
+    EXPECT_EQ(g_assertTrueCallCount, 1);
+    EXPECT_EQ(g_assertFalseCallCount, 0);
+    EXPECT_EQ(vm->state, BBZVM_STATE_READY);
+    EXPECT_EQ(vm->error, BBZVM_ERROR_NONE);
+}
