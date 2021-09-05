df467b377d7c0ad096ee076e57c58e5aa43cac12
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-09-05 15:58:14 -0400

    Bump propolis

--- cmake/propolis/common.cmake
+++ cmake/propolis/common.cmake
@@ -13,7 +13,7 @@ function(propolis_fetch_populate)
         ${PROJECT_NAME}_propolis
 
         GIT_REPOSITORY  https://github.com/SwarmUS/Propolis
-        GIT_TAG         cdb27db977dc7d684827d34d18d3f0357e6e2770
+        GIT_TAG         a7585a3
         GIT_PROGRESS    TRUE
     )
 
