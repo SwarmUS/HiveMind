edf749c34ad0ca1f4112e5e81a5e0d039d0b87a1
xgroleau <xavgroleau@gmail.com>
2021-09-17 12:41:09 -0400

    fix comment

--- src/bittybuzz/include/bittybuzz/BittyBuzzUIFunctions.h
+++ src/bittybuzz/include/bittybuzz/BittyBuzzUIFunctions.h
@@ -3,10 +3,13 @@
 
 #include <bittybuzz/BittyBuzzSystem.h>
 
-/**@brief Buzz library that provides functions for the user to interact with the UI of the platform
- * (LED, Hex display, etc)*/
+/**
+ * @brief Buzz library that provides functions for the user to interact with the UI of the platform
+ * (LED, Hex display, etc)
+ * */
 namespace BittyBuzzUIFunctions {
-    /**@brief Sets the user LED on or off
+    /**
+     *@brief Sets the user LED on or off
      *@b Signature set_led(arg1)
      *@details This closure expects an epxression. all ints that are not 0 evaluates to true (led
      *on), all other types evaluates to false (led off)
@@ -20,17 +23,17 @@ namespace BittyBuzzUIFunctions {
      */
     void setLed();
 
-    /**@brief Sets the user sevent segment to a value
+    /**
+     * @brief Sets the user sevent segment to a value
      *@b Signature set_hex(arg1)
-     * @details This closure expects an int and will bound between 0 and 15
-     * @code
+     *@details This closure expects an int and will bound between 0 and 15
+     *@code
      *  ui.set_hex(6); # 0x06
      *  ui.set_hex(10); # 0x0A
      *  ui.set_hex(20); # Bounded 0x0F
      *  ui.set_hex(-30); # Bounded 0x00
      *  ui.set_hex(""); # Type error
-     *@encode
-     * */
+     *@endcode */
     void setHex();
 } // namespace BittyBuzzUIFunctions
 
