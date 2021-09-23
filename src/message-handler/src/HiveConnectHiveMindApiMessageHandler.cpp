83bd1ce53a376e58bdff9cf8f1050d969b943a31
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-09-23 11:46:04 -0400

    Fix for config forwarding

--- src/message-handler/src/HiveConnectHiveMindApiMessageHandler.cpp
+++ src/message-handler/src/HiveConnectHiveMindApiMessageHandler.cpp
@@ -1,8 +1,8 @@
 #include "HiveConnectHiveMindApiMessageHandler.h"
 
 HiveConnectHiveMindApiMessageHandler::HiveConnectHiveMindApiMessageHandler(
-    ICircularQueue<MessageDTO>& hostQueue, ILogger& logger) :
-    m_hostQueue(hostQueue), m_logger(logger) {}
+    ICircularQueue<MessageDTO>& hostQueue, ICircularQueue<MessageDTO>& remoteQueue, ILogger& logger) :
+    m_hostQueue(hostQueue), m_remoteQueue(remoteQueue), m_logger(logger) {}
 
 bool HiveConnectHiveMindApiMessageHandler::handleMessage(uint16_t sourceId,
                                                          uint16_t destId,
@@ -19,6 +19,13 @@ bool HiveConnectHiveMindApiMessageHandler::handleMessage(uint16_t sourceId,
         ResponseDTO response(message.getMessageId(), apiResponse);
         MessageDTO msgDTO(sourceId, destId, response);
         return m_hostQueue.push(msgDTO);
+    }
+
+    // If it contains a network settings options, forward it to HiveConnect
+    if (const auto *req = std::get_if<HiveConnectNetworkConfigSetRequestDTO>(&message.getMessage())) {
+        HiveConnectHiveMindApiDTO api(message.getMessageId(), *req);
+        MessageDTO msgDTO(sourceId, destId, api);
+        return m_remoteQueue.push(msgDTO);
     }
     return false;
 }
