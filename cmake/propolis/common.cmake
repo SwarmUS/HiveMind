108712b86593d5ee4e1b5a0e057084eb68178c62
Charles Quesnel <31734172+cquesnel@users.noreply.github.com>
2021-09-22 20:32:01 -0400

    Update common.cmake

--- cmake/propolis/common.cmake
+++ cmake/propolis/common.cmake
@@ -13,7 +13,7 @@ function(propolis_fetch_populate)
         ${PROJECT_NAME}_propolis
 
         GIT_REPOSITORY  https://github.com/SwarmUS/Propolis
-        GIT_TAG         9fbfa453594409cfa2576c391aed4610700a2644
+        GIT_TAG         2711c28
         GIT_PROGRESS    TRUE
     )
 
