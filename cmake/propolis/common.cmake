c083cc25e0d4cfc9794095bde8ee9c29d360af54
Charles Quesnel <charles.quesnel@usherbrooke.ca>
2021-09-28 16:56:50 -0400

    Fix merge issues

--- cmake/propolis/common.cmake
+++ cmake/propolis/common.cmake
@@ -13,7 +13,7 @@ function(propolis_fetch_populate)
         ${PROJECT_NAME}_propolis
 
         GIT_REPOSITORY  https://github.com/SwarmUS/Propolis
-        GIT_TAG         d281478
+        GIT_TAG         93795afb406bd882efa1eecfe49b907583162086
         GIT_PROGRESS    TRUE
     )
 
