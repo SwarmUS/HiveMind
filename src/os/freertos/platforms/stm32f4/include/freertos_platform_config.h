4521d08fd50590517db1b84b1dd00c05593a5cd9
xgroleau <xavgroleau@gmail.com>
2021-09-29 20:08:18 -0400

    Resetted vtaskdelay to 1000

--- src/os/freertos/platforms/stm32f4/include/freertos_platform_config.h
+++ src/os/freertos/platforms/stm32f4/include/freertos_platform_config.h
@@ -12,7 +12,7 @@ extern uint32_t SystemCoreClock;
 
 /* Clock config that is platform dependent */
 #define configCPU_CLOCK_HZ (SystemCoreClock)
-#define configTICK_RATE_HZ ((TickType_t)100)
+#define configTICK_RATE_HZ ((TickType_t)1000)
 
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
 standard names. */
