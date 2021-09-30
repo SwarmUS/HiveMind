1fa3c3b74f279d09d03440d92a24100435d244da
Charles Quesnel <quesnel.charles@gmail.com>
2021-09-30 08:07:27 -0400

    Format

--- src/bsp/src/stm32/src/interloc/src/DecawaveArray.cpp
+++ src/bsp/src/stm32/src/interloc/src/DecawaveArray.cpp
@@ -14,7 +14,7 @@ bool DecawaveArray::canDoTWR() const { return m_workingDecasLength > 0; }
 
 bool DecawaveArray::canCalculateAngles() const { return m_workingDecasLength >= 3; }
 
-std::array<std::optional<std::reference_wrapper<Decawave>>, DecawaveArray::angleAntennaArraySize>& DecawaveArray::
-    getAngleAntennaArray() {
+std::array<std::optional<std::reference_wrapper<Decawave>>, DecawaveArray::angleAntennaArraySize>&
+DecawaveArray::getAngleAntennaArray() {
     return m_angleAntennaArray;
 }
