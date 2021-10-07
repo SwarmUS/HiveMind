3ecc4c4300bfb3b50bd6ea40d44c69a186816bf0
Samuel Laperriere <s.t.laperriere@gmail.com>
2021-10-07 09:24:30 -0400

    teleop example

--- cmake/propolis/common.cmake
+++ cmake/propolis/common.cmake
@@ -13,7 +13,7 @@ function(propolis_fetch_populate)
         ${PROJECT_NAME}_propolis
 
         GIT_REPOSITORY  https://github.com/SwarmUS/Propolis
-        GIT_TAG         78d4f10
+        GIT_TAG         c70e034566fbe5c0f3c01cd0fbfed0a6c10cff3e
         GIT_PROGRESS    TRUE
     )
 
