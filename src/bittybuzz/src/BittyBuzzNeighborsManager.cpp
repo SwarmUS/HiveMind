4855b38c4b972158253656cbcc31ae006f35fc4a
Xavier Groleau <xavgroleau@gmail.com>
2021-10-25 15:18:39 -0400

    cleanup

--- src/bittybuzz/src/BittyBuzzNeighborsManager.cpp
+++ src/bittybuzz/src/BittyBuzzNeighborsManager.cpp
@@ -2,31 +2,19 @@
 #include "bbzvm.h"
 #include <cstdio>
 
-BittyBuzzNeighborsManager::BittyBuzzNeighborsManager(uint16_t id,
-                                                     const IInterloc& interloc,
+BittyBuzzNeighborsManager::BittyBuzzNeighborsManager(const IInterloc& interloc,
                                                      ICircularQueue<uint16_t>& posUpdateQueue) :
-    m_id(id), m_interloc(interloc), m_posUpdateQueue(posUpdateQueue) {}
+    m_interloc(interloc), m_posUpdateQueue(posUpdateQueue) {}
 
 void BittyBuzzNeighborsManager::updateNeighbors() {
 
-    // uint32_t length = m_posUpdateQueue.getLength();
-    PositionsTable posTable = m_interloc.getPositionsTable();
-    for (uint32_t i = 0; i<posTable.m_positionsLength; i++){
-        const RelativePosition&  pos = posTable.m_positions[i];
-        bbzneighbors_elem_t neighbor;
-        neighbor.robot = pos.m_robotId;
-        neighbor.azimuth = bbzfloat_fromfloat(pos.m_angle);
-        neighbor.distance = bbzfloat_fromfloat(pos.m_distance);
-        bbzneighbors_add(&neighbor);
-    }
-    /**
+    uint32_t length = m_posUpdateQueue.getLength();
     for (uint32_t i = 0; i < length; i++) {
         auto robotId = m_posUpdateQueue.peek();
         if (robotId) {
             m_posUpdateQueue.pop();
             std::optional<RelativePosition> posOpt = m_interloc.getRobotPosition(robotId.value());
             if (posOpt) {
-                printf("%d: UPDATING ID %d \n", m_id, robotId.value().get());
                 RelativePosition& pos = posOpt.value();
                 bbzneighbors_elem_t neighbor;
                 neighbor.robot = robotId.value();
@@ -35,5 +23,5 @@ void BittyBuzzNeighborsManager::updateNeighbors() {
                 bbzneighbors_add(&neighbor);
             }
         }
-    }**/
+    }
 }
